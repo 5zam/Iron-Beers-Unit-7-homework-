de6e417f4e2b8c49c1f05e01401fc421bd932e3b
5zam <zuwina.s.aljabri@gmail.com>
2023-11-14 10:51:45 +0300

    Done

--- src/app/Compontents/new-coffee/new-coffee.component.ts
+++ src/app/Compontents/new-coffee/new-coffee.component.ts
@@ -1,5 +1,5 @@
 import { HttpClient } from '@angular/common/http';
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { CoffeeServiceService } from 'src/app/Services/coffee-service.service';
@@ -10,10 +10,33 @@ import { CoffeeServiceService } from 'src/app/Services/coffee-service.service';
   styleUrls: ['./new-coffee.component.css']
 })
 export class NewCoffeeComponent implements OnInit{
-
-  
+ 
+  coffeeForm: FormGroup;
+  constructor(private formBuilder: FormBuilder, private CoffeeServiceService: CoffeeServiceService) {
+    this.coffeeForm = this.formBuilder.group({
+      name: ['', Validators.required],
+      tagline: ['', Validators.required],
+      description: ['', Validators.required],
+      first_roasted: ['', Validators.required],
+      roaster_tips: ['', Validators.required],
+      strength_level: [1, Validators.required], // Default to 1, as it's a number
+      contributed_by: ['', Validators.required],
+    });
+  }
   ngOnInit(): void {
   }
- 
+  onSubmit() {
+    if (this.coffeeForm.valid) {
+      const formData = this.coffeeForm.value;
+      this.CoffeeServiceService.createNewCoffee(formData).subscribe(
+        response => {
+          console.log('New coffee created successfully!', response);
+        },
+        error => {
+          console.error('Error creating new coffee', error);
+        }
+      );
+    }
+  }
   
 }
